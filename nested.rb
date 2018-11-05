587323efb5d2887320ef8d9981346ef5f4b55f04
Dakota Clark <bannaray95@gmail.com>
2018-11-05 01:32:06 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -104,8 +104,7 @@ def changing_alan
         }
      }
      programmer_hash[:alan_kay][known_for] = "GUI"
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     programmer_hash
      
      
 end
@@ -127,4 +126,5 @@ def adding_to_dennis
         }
      }
      programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash
 end
